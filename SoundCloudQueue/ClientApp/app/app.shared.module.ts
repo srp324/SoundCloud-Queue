dd24a0598536d8d0e5935e74f3a0852f29f35467
sagarpatel <srp324@drexel.edu>
2019-06-03 23:08:09 -0400

    init SoundCloudService

--- SoundCloudQueue/ClientApp/app/app.shared.module.ts
+++ SoundCloudQueue/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { SoundCloudService } from './services/soundcloudservice';
 
 @NgModule({
     declarations: [
@@ -29,6 +30,9 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        SoundCloudService
     ]
 })
 export class AppModuleShared {
