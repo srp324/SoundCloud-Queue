f6100612b777654c0ae7b2c4903d37ff52e6caa0
sagarpatel <srp324@drexel.edu>
2019-06-05 01:34:30 -0400

    Call the search-service's searchTracks

--- src/app/services/search-service/search.service.ts
+++ src/app/services/search-service/search.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import SC from 'soundcloud';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor() {
+    SC.initialize({
+      client_id: 'GYvpZm3S6Z8m7IRExO0VgEi10Y8AoT64',
+      redirect_uri: 'http://example.com/callback'
+    });
+  }
+
+  async searchTracks(query: string, limit: number, offset: number): Promise<any> {
+    return SC.get('/tracks', {
+      q: query, limit, offset
+    });
+  }
 }
